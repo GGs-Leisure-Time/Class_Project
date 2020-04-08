af175150c6711425cddfdf99acac47fff503bb9d
GGs-Leisure-Time <63327516+GGs-Leisure-Time@users.noreply.github.com>
2020-04-08 15:29:43 +0800

    認識欄位field

--- New Unity Project/Assets/Class1.cs
+++ New Unity Project/Assets/Class1.cs
@@ -10,10 +10,26 @@ public class Class1 : MonoBehaviour
     //1.大小寫
     //2.括號成對出現()[]{}''""<>
 
+    [Header("CAR01")]
+    public car car1;
+    [Header("Car02")]
+    public car car2;
+
+
     //Event：在特定時間會以指定次數執行的方法、功能(藍色名稱)
     //開始事件：播放遊戲時執行一次－初始設定
     private void Start()
     {
         print("Hello World");
+
+        //取得 get
+        print("CAR01的CC數" + car1.cc);
+        print("CAR02的重量" + car2.weight);
+        //設定 set
+        car1.cc = 400;
+        car1.weight = 180;
+        car1.brand = "Nisson";
+        car1.brake = true;
+
     }
 }
--- New Unity Project/Assets/car.cs
+++ New Unity Project/Assets/car.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 
 //摘要：描述此內容，會在其他腳本內看到
+//新增的C#放在專案內為藍圖
+//放在場景物件上為實體物件
 /// <summary>
-///
+/// 定義汽車物件
 /// </summary>
 public class car : MonoBehaviour
 {
@@ -13,12 +15,36 @@ public class car : MonoBehaviour
     //煞車 - 是否開啟 - 布林值bool
 
     //定義欄位語法
-    //資料類型 欄位名稱 (指定 值);
+    //修飾詞 資料類型 欄位名稱 (指定 值);
+    //公開 public 允許其他類別存取-顯示在屬性面板(Inspector)
+    //私人 private (預設) 不允許、不顯示
     //預設值 int、float=>0
     //string=>""
     //bool=>false
-    int cc = 2000;
-    float weight = 100.9f;
-    string brand = "BMW";
-    bool brake = false;
+    //欄位屬性 Attribute：Unity用於輔助欄位的功能
+    //語法：[屬性名稱(值)]
+    //標題 [Header("字串")]
+    //提示 [Tooltip("字串")]
+    //範圍 [Range(最小值,最大值)]
+
+    /// <summary>
+    /// 汽車的CC數
+    /// </summary>
+    [Header("汽車CC數"), Range(500,5000)]
+    public int cc = 2000;
+    /// <summary>
+    /// 汽車的重量
+    /// </summary>
+    [Header("汽車重量"), Tooltip("汽車的總重量"), Range(50,350)]
+    public float weight = 100.9f;
+    /// <summary>
+    /// 汽車的品牌
+    /// </summary>
+    [Header("品牌")]
+    public string brand = "BMW";
+    /// <summary>
+    /// 是否煞車
+    /// </summary>
+    [Header("剎車"), Tooltip("汽車目前是否處於剎車狀態")]
+    public bool brake = false;
 }
