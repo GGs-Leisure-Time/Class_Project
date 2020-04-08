d6d855e36bf641578e3c2d19fb562137fd9bb076
GGs-Leisure-Time <63327516+GGs-Leisure-Time@users.noreply.github.com>
2020-04-08 11:53:31 +0800

    新增C#

---
+++ New Unity Project/Assets/Class1.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;  //using表引用UNITY API(函式庫:Unity屬性、功能)
+
+//class 類別
+//語法：class 類別名稱
+// MonoBehaviour 可以將此腳本掛在Unity物件上
+public class Class1 : MonoBehaviour
+{
+    //定義類別成員
+    //C#
+    //1.大小寫
+    //2.括號成對出現()[]{}''""<>
+
+    //Event：在特定時間會以指定次數執行的方法、功能(藍色名稱)
+    //開始事件：播放遊戲時執行一次－初始設定
+    private void Start()
+    {
+        print("Hello World");
+    }
+}
---
+++ New Unity Project/Assets/car.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+//摘要：描述此內容，會在其他腳本內看到
+/// <summary>
+///
+/// </summary>
+public class car : MonoBehaviour
+{
+    //欄位field - 儲存物件資料
+    //cc數 - 2000     - 整數int
+    //重量 - 100.9    - 浮點數float
+    //品牌 - BMW      - 字串string
+    //煞車 - 是否開啟 - 布林值bool
+
+    //定義欄位語法
+    //資料類型 欄位名稱 (指定 值);
+    //預設值 int、float=>0
+    //string=>""
+    //bool=>false
+    int cc = 2000;
+    float weight = 100.9f;
+    string brand = "BMW";
+    bool brake = false;
+}
