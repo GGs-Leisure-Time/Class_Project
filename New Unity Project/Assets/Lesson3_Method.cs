16abe30fe7009af28d6627c7ca6a6666404f6ced
GGs-Leisure-Time <63327516+GGs-Leisure-Time@users.noreply.github.com>
2020-04-28 14:23:56 +0800

    class02more0428

---
+++ New Unity Project/Assets/Lesson3_Method.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Lesson3_Method : MonoBehaviour
+{
+    //prefab-->預製物:預先製作好的物品
+    public Person person1;
+    public Person person2;
+
+    private void Start()
+    {
+        //呼叫person1方法Talk
+        person1.Talk();
+        person2.Talk();
+        //傳回方法可以直接當作傳回類型使用
+        print("HongKongAssHole BMI" + person1.BMI());
+        print("Free HongKong BMI" + person2.BMI());
+        //使用有參數的方法必須填入對應的引數
+        person1.Walk(1, sound:"pongpong");
+        person2.Walk(100,"左邊");
+
+        person1.Attack();
+        person2.Attack("棍棒");
+    }
+
+}
---
+++ New Unity Project/Assets/Practice_Lesson2and3.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+//引用 Unity 介面 命名空間
+//引用 系統 命名空間(系統 API 字串轉整數)
+public class Practice_Lesson2and3 : MonoBehaviour
+{
+    //儲存兩筆數字
+    public string number1;
+    public string number2;
+
+    [Header("文字：輸出結果")]
+    public Text result;
+
+    /// <summary>
+    ///給第一個欄位使用 
+    /// </summary>
+    /// <param name="number">第一個欄位數值</param>
+    public void InputNumber1(string number)
+    {
+        number1 = number;
+    }
+    /// <summary>
+    ///給第二個欄位使用 
+    /// </summary>
+    /// <param name="number">第二個欄位數值</param>
+    public void InputNumber2(string number)
+    {
+        number2 = number;
+    }
+
+    /// <summary>
+    /// 加法
+    /// </summary>
+    public void Add()
+    {
+        //Int32.Parse(字串) - 將字串轉為整數
+        int n1 = Int32.Parse(number1);
+        int n2 = Int32.Parse(number2);
+
+        print("結果：" + ( n1 + n2 ));
+
+        //輸出結果.文字內容 =
+        result.text = "結果：" + (n1 + n2);
+    }
+
+    public void Sub()
+    {
+        int n1 = Int32.Parse(number1);
+        int n2 = Int32.Parse(number2);
+
+        print("結果：" + (n1 - n2));
+
+        //輸出結果.文字內容 =
+        result.text = "結果：" + (n1 - n2);
+    }
+
+    public void Mul()
+    {
+        int n1 = Int32.Parse(number1);
+        int n2 = Int32.Parse(number2);
+
+        print("結果：" + (n1 * n2));
+
+        //輸出結果.文字內容 =
+        result.text = "結果：" + (n1 * n2);
+    }
+        public void Div()
+    {
+        int n1 = Int32.Parse(number1);
+        int n2 = Int32.Parse(number2);
+
+        print("結果：" + (n1 / n2));
+
+        //輸出結果.文字內容 =
+        result.text = "結果：" + (n1 / n2);
+    }
+    public void Rem()
+    {
+        int n1 = Int32.Parse(number1);
+        int n2 = Int32.Parse(number2);
+
+        print("結果：" + (n1 % n2));
+
+        //輸出結果.文字內容 =
+        result.text = "結果：" + (n1 % n2);
+    }
+
+}
